f1437167f00c4292fd66fea6846957c97ba4b822
Nicholas Kostelnik <nkostelnik@gmail.com>
2013-11-28 15:01:58 +0000

    go on holiday ffs

--- NextBus/MasterViewController.m
+++ NextBus/MasterViewController.m
@@ -23,29 +23,14 @@
 
 @synthesize mapView;
 
-- (void)awakeFromNib {
-  if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-//      self.clearsSelectionOnViewWillAppear = NO;
-      self.preferredContentSize = CGSizeMake(320.0, 600.0);
-  }
-  
-  [super awakeFromNib];
-}
-
 - (void)viewDidLoad {
   [super viewDidLoad];
+  
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(findUser) name:UIApplicationDidBecomeActiveNotification object:nil];
   
   self.detailViewController = (DetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
   
   _stops = [[NSMutableArray alloc] init];
-  _locationUpdates = 0;
-  
-  {
-    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(51.507222,-0.1275);
-    MKCoordinateSpan span = MKCoordinateSpanMake(0.3, 0.3);
-    MKCoordinateRegion zoomRegion = MKCoordinateRegionMake(coordinate, span);
-    [mapView setRegion:zoomRegion animated:NO];
-  }
   
   {
     NSURL* host = [NSURL URLWithString:@"http://countdown.tfl.gov.uk"];
@@ -60,9 +45,34 @@
   {
     locationManager = [[CLLocationManager alloc] init];
     locationManager.delegate = self;
-#ifndef TARGET_IPHONE_SIMULATOR
+  }
+  
+  {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(51.507222,-0.1275);
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.3, 0.3);
+    MKCoordinateRegion zoomRegion = MKCoordinateRegionMake(coordinate, span);
+    [mapView setRegion:zoomRegion animated:NO];
+  }
+
+}
+
+- (void)findUser {
+  
+  {
+    _locationUpdates = 0;
+  }
+  
+  {
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(51.507222,-0.1275);
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.3, 0.3);
+    MKCoordinateRegion zoomRegion = MKCoordinateRegionMake(coordinate, span);
+    [mapView setRegion:zoomRegion animated:YES];
+  }
+
+  {
+//  #ifndef TARGET_IPHONE_SIMULATOR
     [locationManager startUpdatingLocation];
-#endif
+//  #endif
   }
 }
 
@@ -70,18 +80,24 @@
   [self refreshMap:theMapView.region.center];
 }
 
-- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(BusStopAnnotation*)annotation {
-  MKAnnotationView *annotationView =  [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"BusStop"];
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+  if([annotation isKindOfClass:[BusStopAnnotation class]]) {
+    MKAnnotationView *annotationView =  [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"BusStop"];
+    
+    BusStopAnnotation* stopAnnotation = (BusStopAnnotation*)annotation;
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeInfoLight];
+    button.frame = CGRectMake(0, 0, 23, 23);
+    button.tag = [_stops indexOfObject:stopAnnotation.busStop];
+    [button addTarget:self action:@selector(stopSelected:) forControlEvents:UIControlEventTouchUpInside];
+    annotationView.rightCalloutAccessoryView = button;
+    
+    annotationView.canShowCallout = YES;
+    
+    return annotationView;
+  }
   
-  UIButton *button = [UIButton buttonWithType:UIButtonTypeInfoLight];
-  button.frame = CGRectMake(0, 0, 23, 23);
-  button.tag = [_stops indexOfObject:annotation.busStop];
-  [button addTarget:self action:@selector(stopSelected:) forControlEvents:UIControlEventTouchUpInside];
-  annotationView.rightCalloutAccessoryView = button;
-  
-  annotationView.canShowCallout = YES;
-  
-  return annotationView;
+  return nil;
 }
 
 - (void)stopSelected:(id)sender {
@@ -92,7 +108,7 @@
   // meters a way = 111km in a degree
   float metersToDegrees = 1.0f / 111000.0f;
   
-  float degreesDelta = 300.0f * metersToDegrees;
+  float degreesDelta = 1000.0f * metersToDegrees;
   float swLat = position.latitude - degreesDelta;
   float swLng = position.longitude - degreesDelta;
   float neLat = position.latitude + degreesDelta;
@@ -118,25 +134,26 @@
   [[RKObjectManager sharedManager] addResponseDescriptor:stopDescriptor];
   
   [[RKObjectManager sharedManager] getObjectsAtPath:pathPattern
-                                         parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
-                                           NSArray* results = [mappingResult array];
-                                           [_stops removeAllObjects];
-                                           [_stops addObjectsFromArray:results];
-                                           
-                                           for (id annotation in mapView.annotations) {
-                                             [mapView removeAnnotation:annotation];
-                                           }
-                                           
-                                           for (BusStop* stop in _stops) {
-                                             CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(stop.latitude, stop.longitude);
-                                             NSString* title = stop.indicator ? [NSString stringWithFormat:@"%@ - %@", stop.indicator, stop.name] : stop.name;
-                                             BusStopAnnotation* annotation = [[BusStopAnnotation alloc] initWithCoordinate:coordinate andTitle:title andBusStop:stop];
-                                             [mapView addAnnotation:annotation];
-                                           }
-                                           
-                                         } failure:^(RKObjectRequestOperation *operation, NSError *error) {
-                                           NSLog(@"Load failed");
-                                         }];
+                                         parameters:nil
+                                            success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
+                                         NSArray* results = [mappingResult array];
+                                         [_stops removeAllObjects];
+                                         [_stops addObjectsFromArray:results];
+                                         
+                                         for (id annotation in mapView.annotations) {
+                                           [mapView removeAnnotation:annotation];
+                                         }
+                                         
+                                         for (BusStop* stop in _stops) {
+                                           CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(stop.latitude, stop.longitude);
+                                           NSString* title = stop.indicator ? [NSString stringWithFormat:@"%@ - %@", stop.indicator, stop.name] : stop.name;
+                                           BusStopAnnotation* annotation = [[BusStopAnnotation alloc] initWithCoordinate:coordinate andTitle:title andBusStop:stop];
+                                           [mapView addAnnotation:annotation];
+                                         }
+                                         
+                                       } failure:^(RKObjectRequestOperation *operation, NSError *error) {
+                                         NSLog(@"Load failed");
+                                       }];
 }
 
 - (void)locationManager:(CLLocationManager *)manager
@@ -146,7 +163,7 @@
     CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(newLocation.coordinate.latitude, newLocation.coordinate.longitude);
     MKCoordinateSpan span = MKCoordinateSpanMake(0.01, 0.01);
     MKCoordinateRegion zoomRegion = MKCoordinateRegionMake(coordinate, span);
-    [mapView setRegion:zoomRegion animated:NO];
+    [mapView setRegion:zoomRegion animated:YES];
     
     [self refreshMap:coordinate];
   }
